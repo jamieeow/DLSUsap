84d18db17736e3c7e28115172b201ba17422d70b
Jamie <57598321+jamieeow@users.noreply.github.com>
2020-09-10 00:35:19 +0800

    Initial commit

---
+++ src/ChatScreen/Controller.java
@@ -0,0 +1,25 @@
+package ChatScreen;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class Controller {
+
+
+    @FXML Button btnImg, btnSend, btnLogout;
+    @FXML TextField txtfieldInput;
+    @FXML VBox vboxDialog;
+    @FXML Label lblName;
+
+    public void initialize(){
+
+    }
+
+    public void handleSend() {
+        
+    }
+
+}
